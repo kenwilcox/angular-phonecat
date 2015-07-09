cd576507f1417f5b2f70e83640fc18e0dc2f49c6
Ken Wilcox <wilcoxk@medamerica.com>
2015-07-08 23:13:42 -0600

    using splice to limit the data

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -4,10 +4,11 @@
 
 var phonecatApp = angular.module('phonecatApp', []);
 
-phonecatApp.controller('PhoneListCtrl', ['$scope', '$http', function($scope, $http) {
-  $http.get('phones/phones.json').success(function(data) {
-    $scope.phones = data;
-  });
+phonecatApp.controller('PhoneListCtrl', ['$scope', '$http', 
+  function($scope, $http) {
+    $http.get('phones/phones.json').success(function(data) {
+      $scope.phones = data.splice(0,5);
+    });
 
-  $scope.orderProp = 'age';
+    $scope.orderProp = 'age';
 }]);
