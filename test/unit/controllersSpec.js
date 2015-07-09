ef6bdeb9c58b3ee7b042f2deeb76b8517ec5a5cb
Ken Wilcox <wilcoxk@medamerica.com>
2015-07-08 23:13:59 -0600

    end of step 5

--- test/unit/controllersSpec.js
+++ test/unit/controllersSpec.js
@@ -5,8 +5,13 @@ describe('PhoneCat controllers', function() {
 
   describe('PhoneListCtrl', function(){
     var scope, ctrl, $httpBackend;
-
+    
+    // Load out app module definitions before each test
     beforeEach(module('phonecatApp'));
+    
+    // The injector ignores leading and trailing underscores here (i.e. _$httpBackend_).
+    // This allows us to inject a service but then attach it to a variable
+    // with the same name as the service in order to avoid a name conflict.
     beforeEach(inject(function(_$httpBackend_, $rootScope, $controller) {
       $httpBackend = _$httpBackend_;
       $httpBackend.expectGET('phones/phones.json').
